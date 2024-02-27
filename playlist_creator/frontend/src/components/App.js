b2604c158e93d07f3a861b2119867a1ae6b9cf3b
Nathan Vogt <merrick.vogt@gmail.com>
2024-02-27 16:36:24 -0500

    browser router setup

--- playlist_creator/frontend/src/components/App.js
+++ playlist_creator/frontend/src/components/App.js
@@ -3,15 +3,17 @@ import { createRoot } from "react-dom/client";
 import HomePage from "./HomePage";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World, This is The Playlist Creator</h1>
-      {/* <HomePage />
-      <RoomJoinPage />
-      <CreateRoomPage /> */}
+      <h1>Hello Me, This is The Playlist Creator</h1>
+      <Routes>
+        <Route exact path="/" element={<p>This is the home page.</p>} />
+        <Route path="/join" element={<RoomJoinPage />} />
+        <Route path="/create" element={<CreateRoomPage />} />
+      </Routes>
     </div>
   );
 };
--- playlist_creator/frontend/src/components/HomePage.js
+++ playlist_creator/frontend/src/components/HomePage.js
@@ -1,21 +1,15 @@
 import React from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const HomePage = () => {
-    return (
-        <div>
-            <Router>
-                <Switch>
-                    <Route exact path="/" element={<p>This is the home page.</p>} />
-                    <Route path="/join" element={RoomJoinPage} />
-                    <Route path="/create" element={CreateRoomPage} />
-                </Switch>
-            </Router>
-            <h1>This is the Home Page</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      
+      <h1>This is the Home Page</h1>
+    </div>
+  );
+};
 
 export default HomePage;
